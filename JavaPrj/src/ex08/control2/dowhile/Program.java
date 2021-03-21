723fc9a498cf4adf4089bbf0758f8bc708fa5f6f
HiiWee <wpdlzhf159@gmail.com>
2021-03-22 02:16:43 +0900

    [Add File]ex08

---
+++ JavaPrj/src/ex08/control2/dowhile/Program.java
@@ -0,0 +1,79 @@
+package ex08.control2.dowhile;
+
+//import Class "Scanner"
+import java.util.Scanner;
+
+public class Program {
+	public static void main(String[] args) {
+
+		int kor1, kor2, kor3;
+		int total;
+		float avg;
+
+		Scanner scan = new Scanner(System.in);
+		kor1 = 0;
+		kor2 = 0;
+		kor3 = 0;
+		
+		while(true) {
+			//----------------------성적 입력 부분----------------------
+			System.out.println("┌ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ┐");
+			System.out.println("│         성적입력       │");
+			System.out.println("└ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ┘");
+			
+			//국어1 성적 입력부분
+			do {
+				System.out.print("국어1:");
+				kor1 = scan.nextInt();
+				if(kor1<0 || kor1>100) {
+					System.out.println("성적범위(0~100)를 벗어났습니다.");
+				}
+			} while(kor1<0 || kor1>100);
+			
+			//국어2 성적 입력부분
+			do {
+				System.out.print("국어2:");
+				kor2 = scan.nextInt();
+				if(kor2<0 || kor2>100) {
+					System.out.println("성적범위(0~100)를 벗어났습니다.");
+				}
+			} while(kor2<0 || kor2>100);
+			
+			//국어3 성적 입력부분
+			do {
+				System.out.print("국어3:");
+				kor3 = scan.nextInt();
+				
+				if(kor3<0 || kor3>100) {
+					System.out.println("성적범위(0~10)을 벗어났습니다.");
+				}
+			} while(kor3<0 || kor3>100);
+			
+			// 유효검사 (0~100 사이값만)
+			//int validate = (0<=kor1 && 100>=kor1)?1:0;
+			//System.out.println(validate);
+			
+			
+			
+			//----------------------성적 출력 부분----------------------
+			
+			total = kor1 + kor2 + kor3;
+			//소수점 살리기(묵시적 형변환)
+			avg = (float)(total / 3.0);  //or use "avg = total / 3.0f"
+			
+			System.out.println("┌ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ┐");
+			System.out.println("│         성적출력       │");
+			System.out.println("└ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ┘");
+	
+			//argument index
+			//System.out.printf("%2$d %1$d %3$d\n", 1, 2, 3);
+			//formating
+			System.out.printf("국어1 : %3d\n", kor1);
+			System.out.printf("국어2 : %3d\n", kor2);
+			System.out.printf("국어3 : %3d\n", kor3);
+			System.out.printf("총점 : %3d\n", total);
+			System.out.printf("평균 : %6.2f\n", avg);
+			System.out.println("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ");
+		}
+	}
+}
