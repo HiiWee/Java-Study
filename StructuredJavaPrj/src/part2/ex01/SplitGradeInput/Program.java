413d00fde3729abcbf019b65c78108e0755bb815
HiiWee <wpdlzhf159@gmail.com>
2021-03-24 19:10:27 +0900

    [Add File] Program.java

---
+++ StructuredJavaPrj/src/part2/ex01/SplitGradeInput/Program.java
@@ -0,0 +1,18 @@
+package part2.ex01.SplitGradeInput;
+
+import java.util.Scanner;
+
+public class Program {
+	public static void main(String[] args) {
+		int [] kors = new int[3];
+		
+		int total;
+		float avg;
+		int menu;
+		boolean keepLoop = true;
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.print("stop");
+	}
+}
