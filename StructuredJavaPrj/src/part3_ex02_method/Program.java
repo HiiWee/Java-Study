58491c21cff9fc06d2b56399e74c1f641b2b519a
HiiWee <wpdlzhf159@gmail.com>
2021-04-29 13:17:04 +0900

    Add: object-oriented file for input grade
    this is Object-Oriented version of part3_ex01_encapsulation

---
+++ StructuredJavaPrj/src/part3_ex02_method/Program.java
@@ -0,0 +1,61 @@
+package part3_ex02_method;
+
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		ExamList list = new ExamList();
+		//list.init();
+		
+		int menu;
+		boolean keepLoop = true;
+		// 스캐너는 굳이 전역변수로 공유할필요없다.
+		Scanner scan = new Scanner(System.in);
+
+		while (keepLoop) {
+			menu = inputMenu();
+
+			switch (menu) {
+			case 1:
+				//ExamList.inputList(list);
+				list.inputList();
+				break;
+			case 2: 
+				//ExamList.printList(list);
+				list.printList();
+				break;
+			case 3:
+				System.out.println("Bye~~");
+
+				keepLoop = false;
+				break;
+
+			default:
+				System.out.println("잘못된 값을 입력하셨습니다. 메뉴는 1~3까지입니다.");
+
+			}
+
+		}
+	}
+
+	
+	static int inputMenu() {
+
+		int menu;
+
+		Scanner scan = new Scanner(System.in);
+		System.out.println("┌───────────────────────────┐");
+		System.out.println("│           메인 메뉴       │");
+		System.out.println("└───────────────────────────┘");
+		System.out.println("\t1. 성적입력 ");
+		System.out.println("\t2. 성적출력 ");
+		System.out.println("\t3. 종료 ");
+		System.out.println("\t선택> ");
+		menu = scan.nextInt();
+
+		return menu;
+	}
+
+}
