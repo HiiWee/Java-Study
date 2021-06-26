51a6d3a416956568b5f3ef6df1d904a2109e072a
HiiWee <wpdlzhf159@gmail.com>
2021-06-26 14:42:25 +0900

    Add: file, nothing change

---
+++ sgLove/src/Ch03/Ex03_27.java
@@ -0,0 +1,23 @@
+package ch03;
+
+import java.util.Scanner;
+
+public class Ex03_27 {
+	public static void main(String[] args) {
+		String str1 = "아! 대한민국 ";
+		String str2 = "Korea";
+		
+		System.out.println(str1 + str2);
+		System.out.println("아! 대한민국 \n Korea");
+		
+		int a = 1000;
+		int b = 2000;
+		
+		System.out.println(str1 + a + b + "리 금수강산");
+		System.out.println(str1 + (a + b) + "리 금수강산");
+		System.out.println(a + b + str1 + "리 금수강산");
+		System.out.println( '3' + "천리 금수강산");
+		System.out.println( '3' + 2997 + "리 금수강산");
+		
+	}
+}
