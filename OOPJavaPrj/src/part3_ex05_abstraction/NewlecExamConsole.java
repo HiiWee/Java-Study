222df64ae1d06a703577a654a2c0b7c72e673bf6
HiiWee <wpdlzhf159@gmail.com>
2021-07-05 02:27:59 +0900

    Edit: 메소드 관련 주석

--- OOPJavaPrj/src/part3_ex05_abstraction/NewlecExamConsole.java
+++ OOPJavaPrj/src/part3_ex05_abstraction/NewlecExamConsole.java
@@ -2,27 +2,29 @@ package part3_ex05_abstraction;
 
 import java.util.Scanner;
 
-public class NewlecExamConsole extends ExamConsole{
+public class NewlecExamConsole extends ExamConsole {
 
+	// 팩토리 메소드의 구현
 	@Override
 	protected Exam makeExam() {
 		return new NewlecExam();
 	}
 
+	// 이벤트 메소드 구현
 	@Override
 	protected void onPrint(Exam exam) {
 		int com;
-		NewlecExam newlecExam = (NewlecExam)exam;
+		NewlecExam newlecExam = (NewlecExam) exam;
 		com = newlecExam.getCom();
 		System.out.printf("컴퓨터 %d\n", com);
 
-		
 	}
 
+	// 이벤트 메소드 구현
 	@Override
 	protected void onInput(Exam exam) {
-		NewlecExam temp = (NewlecExam)exam;
-		
+		NewlecExam newlecExam = (NewlecExam) exam;
+
 		Scanner scan = new Scanner(System.in);
 		int com;
 
@@ -34,7 +36,7 @@ public class NewlecExamConsole extends ExamConsole{
 				System.out.println("컴퓨터성적은 0~100까지의 범위만 입력이 가능합니다.");
 
 		} while (com < 0 || 100 < com);
-		temp.setCom(com);
+		newlecExam.setCom(com);
 	}
 
 }
