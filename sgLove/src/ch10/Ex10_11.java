de838bbb35792f6b444f6a0a5bc39ee497e05e0b
HiiWee <wpdlzhf159@gmail.com>
2021-07-06 14:24:00 +0900

    Add: week14 교재 예제

---
+++ sgLove/src/ch10/Ex10_11.java
@@ -0,0 +1,42 @@
+package ch10;
+
+class SD1 {
+	public int i1;
+	public double d1;
+	public SD1(int i1) {
+		System.out.println("SD1(int i1) 생성자 수행");
+		this.i1 = i1 * i1;
+		System.out.println(i1 + "의 2제곱은 : " + this.i1);
+	}
+
+	public SD1(double d1) {
+		System.out.println("SD1(double d1) 생성자 수행");
+		this.d1 = d1 * d1;
+		System.out.println(i1 + "의 2제곱은 : " + this.d1);
+	}
+}
+
+class Sub1 extends SD1{
+	public Sub1(int i1) {
+		super(i1);
+		System.out.println("Sub1(int i1) 생성자 수행");
+		this.i1 = i1 * i1 * i1;
+		System.out.println(i1 + "의 3제곱은 : " + this.i1);
+	}
+	
+	public Sub1(double d1) {
+		super(d1);
+		System.out.println("Sub1(double d1) 생성자 수행");
+		this.d1 = d1 * d1 * d1;
+		System.out.println(i1 + "의 3제곱은 : " + this.d1);
+	}
+}
+
+public class Ex10_11 {
+
+	public static void main(String[] args) {
+		Sub1 ob1 = new Sub1(10);
+		Sub1 ob2 = new Sub1(10.5);
+	}
+
+}
