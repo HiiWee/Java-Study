4828d3154abb7b9ed164535719a1a022f1541c03
HiiWee <wpdlzhf159@gmail.com>
2021-07-14 14:16:58 +0900

    Add: 기능 구현 클래스파일

---
+++ OOPJavaPrj/src/part3_ex06_interface/A.java
@@ -0,0 +1,19 @@
+package part3_ex06_interface;
+
+public class A {
+	private X x;
+	
+	public void setX(X x) {
+		this.x = x;
+	}
+
+	public A() {
+		//b = new B();
+	}
+	
+	public void print() {
+		int total = x.total();
+		
+		System.out.println("total is " + total);
+	}
+}
