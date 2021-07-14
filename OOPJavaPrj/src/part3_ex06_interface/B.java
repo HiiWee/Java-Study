fba11cff3a587bfc54292afa90dc7716cc2e6214
HiiWee <wpdlzhf159@gmail.com>
2021-07-14 14:17:53 +0900

    Add: 인터페이스 implementes 파일

---
+++ OOPJavaPrj/src/part3_ex06_interface/B.java
@@ -0,0 +1,9 @@
+package part3_ex06_interface;
+
+public class B implements X {
+
+	@Override
+	public int total() {
+		return 30;
+	}
+}
---
+++ OOPJavaPrj/src/part3_ex06_interface/C.java
@@ -0,0 +1,10 @@
+package part3_ex06_interface;
+
+public class C implements X{
+
+	@Override
+	public int total() {
+		return 50;
+	}
+	
+}
