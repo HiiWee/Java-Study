eb06c4f8b66f0ddf825dbeaa67f42f0014dc7f9e
HiiWee <wpdlzhf159@gmail.com>
2021-11-17 01:35:15 +0900

    테스트를 위한 변경

--- JDBCPrj/src/com/hoseok/app/console/NoticeConsole.java
+++ JDBCPrj/src/com/hoseok/app/console/NoticeConsole.java
@@ -2,6 +2,7 @@ package com.hoseok.app.console;
 
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Scanner;
 
 import com.hoseok.app.entity.Notice;
 import com.hoseok.app.service.NoticeService;
@@ -15,7 +16,7 @@ public class NoticeConsole {
 	}
 	
 	public void printNoticeList() throws ClassNotFoundException, SQLException {
-		List<Notice> list = service.getList();
+		List<Notice> list = service.getList(1);
 		System.out.printf("───────────────────────────────────\n");
 		System.out.printf("<공지사항> 총 %d 게시글\n", 12);
 		System.out.printf("───────────────────────────────────\n");
@@ -32,8 +33,20 @@ public class NoticeConsole {
 	}
 
 	public int inputNoticeMenu() {
+		Scanner scan = new Scanner(System.in);
+		// 사용자 입력을 받기위한 메뉴 출력
+		System.out.print("1.상세조회/ 2.이전/ 3.다음/ 4.글쓰기/ 5.종료>");
 		
-		return 0;
+		// nextInt()는 두가지 문제를 야기, 
+		//(1. 정수값이 아닌 값을 입력,))
+		//2. 버퍼에 남아있는 엔터키의 키값이 다음 입력에 영향을 줌)
+		// 따라서 문자열로 받아 변환을해 입력에 이용
+		String menu_ = scan.nextLine();
+
+		// 원래는 변환될 수 있는 것인지 체크하는 과정(숫자맞는지, 잘못된값 아닌지)필요하지만 생략
+		int menu = Integer.parseInt(menu_);
+		
+		return menu;
 	}
 
 }
