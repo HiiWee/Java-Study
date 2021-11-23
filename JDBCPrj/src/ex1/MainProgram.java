d68adcba76df0e481c303134d2b5b5e8bacc3456
HiiWee <wpdlzhf159@gmail.com>
2021-11-24 01:49:04 +0900

    검색 이후 처음으로 돌아가기 위한 선택지 추가

--- JDBCPrj/src/ex1/MainProgram.java
+++ JDBCPrj/src/ex1/MainProgram.java
@@ -1,4 +1,4 @@
-package ex1;
+package ex1;//(view, run 섞여있음)
 
 import java.sql.SQLException;
 
@@ -10,6 +10,7 @@ public class MainProgram {
 		NoticeConsole console = new NoticeConsole();
 		
 		// EXIT 라벨을 이용한 반복문 한번에 벗어나기
+		// controller가 따로 존재하지 않고 직접적으로 view에서 호출함
 		EXIT: while (true) {
 			console.printNoticeList();
 			int menu = console.inputNoticeMenu();
@@ -28,7 +29,10 @@ public class MainProgram {
 			case 5: // 검색
 				console.inputSearchWord();
 				break;
-			case 6: // 종료
+			case 6: // 뒤로가기
+				console.movePrevSearch();
+				break;
+			case 7:
 				System.out.println("End the program");
 				break EXIT;
 			default:
